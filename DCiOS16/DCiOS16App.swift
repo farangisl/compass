686afdb1e65db5fbfd34c916981c9a87d4d2bcc8
Farangis <Farangis>
2023-04-05 08:45:51 +0500

    Initial Commit

---
+++ DCiOS16/DCiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DCiOS16App.swift
+//  DCiOS16
+//
+//  Created by Махмадёрова Фарангис Шухратовна on 05.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DCiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
