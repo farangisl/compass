6d7447c4e3233a910f45cc27a66bf26317fc2d2b
Farangis <Farangis>
2023-04-07 11:53:29 +0500

    second commit

---
+++ DCiOS16/ChartView.swift
@@ -0,0 +1,63 @@
+//
+//  ChartView.swift
+//  DCiOS16
+//
+//  Created by Махмадёрова Фарангис Шухратовна on 06.04.2023.
+//
+
+import SwiftUI
+import Charts
+
+struct ChartView: View {
+    var body: some View {
+        Chart {
+            ForEach(data) { item in
+                LineMark(x: .value("Day", item.day), y: .value("Value", item.value), series: .value("Year", "2023"))
+                    .cornerRadius(10)
+                    .interpolationMethod(.catmullRom)
+                    .foregroundStyle(by: .value("Year", "2023"))
+                    .symbol(by: .value("Year", "2023"))
+                
+            }
+            
+            ForEach(data2) { item in
+                LineMark(x: .value("Day", item.day), y: .value("Value", item.value), series: .value("Year", "2022"))
+                    .cornerRadius(10)
+                    .interpolationMethod(.catmullRom)
+                    .foregroundStyle(by: .value("Year", "2022"))
+                    .symbol(by: .value("Year", "2022"))
+
+            }
+        }
+        .frame(height: 300)
+        .padding(20)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
+
+struct Value: Identifiable {
+    var id = UUID()
+    var day: String
+    var value: Double
+}
+
+let data = [
+    Value(day: "Jun 1", value: 200),
+    Value(day: "Jun 2", value: 96),
+    Value(day: "Jun 3", value: 312),
+    Value(day: "Jun 4", value: 256),
+    Value(day: "Jun 5", value: 505),
+]
+
+let data2 = [
+    Value(day: "Jun 1", value: 151),
+    Value(day: "Jun 2", value: 192),
+    Value(day: "Jun 3", value: 176),
+    Value(day: "Jun 4", value: 158),
+    Value(day: "Jun 5", value: 401),
+]
--- DCiOS16/ContentView.swift
+++ DCiOS16/ContentView.swift
@@ -9,10 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State var showMenu = false
-    @State var selectedMenu: Menu = .compass
+    @AppStorage("selectedMenu") var selectedMenu: Menu = .compass
+    @GestureState var press = false
+    
+    var longPress: some Gesture {
+        LongPressGesture(minimumDuration: 1)
+            .updating($press) { currentState, gestureState, transaction in
+                gestureState = currentState
+            }
+            .onEnded { value in
+                showMenu = true
+            }
+    }
     
     var body: some View {
         ZStack {
+            Color(.systemBackground).ignoresSafeArea()
+            
             switch selectedMenu {
             case .compass:
                 MessageView()
@@ -25,18 +38,16 @@ struct ContentView: View {
             case .gooey:
                 Text("Gooey")
             case .charts:
-                Text("Charts")
+                DetailView()
             case .halfsheet:
-                Text("Half Sheet")
+                MenuView()
             }
-            
-            Button("Show Menu") {
-                showMenu = true
-            }
-            .sheet(isPresented: $showMenu) {
-                MenuView(selectedMenu: $selectedMenu)
-                    .presentationDetents([.medium, .large])
-            }
+        }
+        .onTapGesture {}
+        .gesture(longPress)
+        .sheet(isPresented: $showMenu) {
+            MenuView()
+                .presentationDetents([.medium, .large])
         }
     }
 }
